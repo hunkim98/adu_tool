6706871d84ba637535dae1514de3a92f98b54a72
Hun Kim <hunkim98@gmail.com>
2025-04-17 13:25:33 -0400

    Update basepath

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: isProd ? "/adu_tool/" : "",
+  basePath: isProd ? "/adu_tool" : "",
   output: "export",
   reactStrictMode: true,
 };
